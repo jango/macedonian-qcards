926c76ef9e1b304aa78d2ae242988ba462d88aab
Nikita Pchelin <nikita.pchelin@gmail.com>
2013-01-26 15:27:48 -0500

    newely rendered files for all sets, and updated config file

---
+++ output/generate_pdfs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 06.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 07.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 04.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 05.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 02.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 03.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 13.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 01.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 14.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 12.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 15.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 08.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 09.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 11.tex"
+latex -interaction=nonstopmode --output-format=pdf --output-directory="/home/jango/workspace/macedonian-qcards/output" "/home/jango/workspace/macedonian-qcards/output/Set 10.tex"
+rm *.aux
+rm *.log
